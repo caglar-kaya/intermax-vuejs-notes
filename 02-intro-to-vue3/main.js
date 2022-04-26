1d1f468ceab993dbc55515258e4472b00da9f545
Caglar Kaya <caglar.kaya.europe@gmail.com>
2022-04-26 16:01:53 +0200

    conditional rendering

--- 02-intro-to-vue3/main.js
+++ 02-intro-to-vue3/main.js
@@ -1,9 +1,10 @@
 const app = Vue.createApp({
   data() {
-    return {
-      product: 'Socks',
-      image: './assets/images/socks_green.jpg',
-      url: 'https://vuejs.org/'
-    };
-  },
-});
+      return {
+          product: 'Socks',
+          image: './assets/images/socks_blue.jpg',
+          inventory: 3,
+          onSale: true
+      }
+  }
+})
